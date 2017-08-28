f0d41aa0262a4fb8a944941ad4f9dee3146c1e96
gabidavila <gabidavila@gmail.com>
2017-08-28 15:48:32 -0400

    shopping

--- index.js
+++ index.js
@@ -1,30 +1,91 @@
 var cart = [];
 
 function getCart() {
- return cart;
+    return cart;
 }
 
 function setCart(c) {
-  cart = c;
-  return cart;
+    cart = c;
+    return cart;
 }
 
 function addToCart(item) {
- // write your code here
+    let price = Math.floor(Math.random() * 100);
+    cart.push({[item]: price});
+    console.log(`${item} has been added to your cart.`);
+    return cart;
+}
+
+function printTwoItems() {
+    let items = []
+    for (let i = 0; i < cart.length; i++) {
+        let itemName = Object.keys(cart[i])[0];
+        let item = `${itemName} at $${cart[i][itemName]}`;
+        items.push(item);
+    }
+    return "In your cart, you have " + items.join(" and ");
+}
+
+function printMultipleItems() {
+    let items = ""
+
+    for (let j = 0; j < cart.length; j++) {
+        let itemName = Object.keys(cart[j])[0];
+        let item;
+
+        if (j != cart.length - 1) {
+            item = `${itemName} at $${cart[j][itemName]}, `;
+        } else {
+            item = `and ${itemName} at $${cart[j][itemName]}`;
+        }
+        items += item
+    }
+    return "In your cart, you have " + items;
 }
 
 function viewCart() {
-  // write your code here
+    let cartDescription = ""
+
+    switch (cart.length) {
+        case 0:
+            cartDescription = "Your shopping cart is empty";
+            break;
+        case 1:
+            let itemName = Object.keys(cart[0])[0];
+            let item = `${itemName} at $${cart[0][itemName]}`;
+            cartDescription = "In your cart, you have " + item;
+            break;
+        case 2:
+            cartDescription = printTwoItems();
+            break;
+        default:
+            cartDescription = printMultipleItems();
+            break;
+    }
+    console.log(cartDescription + ".");
 }
 
 function total() {
-  // write your code here
+    let totalSum = 0;
+    for (let i = 0; i < cart.length; i++) {
+        let itemName = Object.keys(cart[i])[0];
+        totalSum += cart[i][itemName];
+    }
+    return totalSum;
 }
 
 function removeFromCart(item) {
-  // write your code here
+    let removed = false;
+    for(let i = 0; i < cart.length; i++) {
+        if (cart[i].hasOwnProperty(item)) {
+            removed = cart.splice(i, 1);
+        }
+    }
+
+    console.log(removed ? "Item removed from your cart" : "That item is not in your cart.");
+    return cart;
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+    // write your code here
 }
